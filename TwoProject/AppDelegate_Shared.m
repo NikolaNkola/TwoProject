673e8bfee1674a696e770adb1b1de6aa4829f20f
Ivanov Ivan <iivanov@mail.ru>
2013-01-26 01:32:33 +0200

    add new files

---
+++ TwoProject/AppDelegate_Shared.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate_Shared.m
+//  TwoProject
+//
+//  Created by Administrator on 26.01.13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate_Shared.h"
+
+@implementation AppDelegate_Shared
+
+@end
