edb7bd8adea32c31f148cb57aad782bd8b71680b
Ivanov Ivan <iivanov@mail.ru>
2013-01-26 01:43:59 +0200

    add AppDelegate_iPhone

---
+++ TwoProject/AppDelegate_iPhone.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate_iPhone.h
+//  TwoProject
+//
+//  Created by Administrator on 26.01.13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate_Shared.h"
+
+@interface AppDelegate_iPhone : AppDelegate_Shared
+
+@end
---
+++ TwoProject/AppDelegate_iPhone.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate_iPhone.m
+//  TwoProject
+//
+//  Created by Administrator on 26.01.13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate_iPhone.h"
+
+@implementation AppDelegate_iPhone
+
+@end
